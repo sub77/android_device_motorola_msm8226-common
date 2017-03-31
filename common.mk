omnirom/android_device_oppo_msm8974-common

common: update usb config files
remove default persist.sys.usb.config property

Change-Id: I6a031f3edc3b51184f75cdcc4bc74cec42595d8e

common.mk
@@ -19,10 +19,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
 
-#Default USB mount
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp
-
 # Live Wallpapers
 PRODUCT_PACKAGES += \
     LiveWallpapers \
@@ -39,6 +35,7 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/init.oppo.usb.rc:root/init.oppo.usb.rc \
     $(COMMON_PATH)/configs/init.oppo.common.rc:root/init.oppo.common.rc \
     $(COMMON_PATH)/configs/init.qcom.power.rc:root/init.qcom.power.rc \
+    $(COMMON_PATH)/configs/init.qcom.usb.sh:root/init.qcom.usb.sh \
     $(COMMON_PATH)/configs/ueventd.qcom.rc:root/ueventd.qcom.rc
 
 # Config files for touch and input

sub77/android_device_motorola_msm8226-common

Signed-off-by: sub77 <sub77@ymail.com>

common.mk
@@ -148,7 +148,8 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/init.mmi.boot.sh:root/init.mmi.boot.sh \
     $(COMMON_PATH)/rootdir/init.mmi.touch.sh:root/init.mmi.touch.sh \
     $(COMMON_PATH)/rootdir/init.qcom.bt.sh:root/init.qcom.bt.sh \
-    $(COMMON_PATH)/rootdir/init.qcom.ril.sh:root/init.qcom.ril.sh
+    $(COMMON_PATH)/rootdir/init.qcom.ril.sh:root/init.qcom.ril.sh \
+    $(COMMON_PATH)/rootdir/init.qcom.usb.sh:root/init.qcom.usb.sh
 
 # Thermal-engine
 PRODUCT_COPY_FILES += \
@@ -156,13 +157,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     libshims_thermal
-
-# USB/adb
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=adb
-
-ADDITIONAL_DEFAULT_PROPERTIES += \
-    ro.adb.secure=0
 
 # USB
 PRODUCT_PACKAGES += \