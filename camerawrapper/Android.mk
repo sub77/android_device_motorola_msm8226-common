713727d51ce2c9b9a9c6358169d734f181275dfd
Simon Shields <simon@lineageos.org>
2017-08-24 00:23:09 +1000

    camerawrapper: add missing libs for headers
    Change-Id: If38b2da4fe5b8dddba7e202165f00b647e614453
    Signed-off-by: sub77 <sub77@ymail.com>

--- camerawrapper/Android.mk
+++ camerawrapper/Android.mk
@@ -1,20 +1,31 @@
+ifneq ($(filter falcon peregrine thea,$(TARGET_DEVICE)),)
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := system/media/camera/include \
-                    system/core/base/include
+LOCAL_C_INCLUDES := \
+    system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
+LOCAL_STATIC_LIBRARIES := libbase libarect
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils
-
-LOCAL_STATIC_LIBRARIES := \
-    libarect
+    libhardware liblog libcamera_client libutils libdl \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE := camera-wrapper.msm8226
+# workaround for hardware/qcom/camera already defining camera.board
+LOCAL_MODULE := camera-wrapper.msm8974
+
+
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+#include $(BUILD_HEAPTRACKED_SHARED_LIBRARY)
+
+$(LOCAL_INSTALLED_MODULE): $(LOCAL_BUILT_MODULE) | $(ACP)
+	@echo "Install (overridden): $@"
+	$(hide) $(ACP) -fp $< $(dir $@)/camera.msm8226$(TARGET_SHLIB_SUFFIX)
+
+endif
