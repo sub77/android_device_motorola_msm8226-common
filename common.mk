70501262181b0f3599a7d26af2d0d34fb9c33b54
sub77 <sub77@ymail.com>
2017-09-06 21:40:06 +0000

    msm8226-common: add hardware service files

--- common.mk
+++ common.mk
@@ -84,14 +84,21 @@ PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl \
     android.hardware.vibrator@1.0-impl \
     android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
     android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service \
     android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service \
     android.hardware.light@2.0-impl \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.usb@1.0-service \
     hwcomposer.msm8226 \
     gralloc.msm8226 \
     copybit.msm8226 \
