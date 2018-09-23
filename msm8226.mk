CypherOS/device_motorola_msm8226-common

msm8226-common: Add dummy build.prop file for HDR init
* For some reason, the Morpho HDR algorithm init process attempts to
  take the hash of /system/build.prop. This is not accessible on Oreo
  anymore, so this causes HDR init to fail.
* Hack the camera library to attempt to hash /vendor/etc/hdrhax instead,
  and provide it a dummy build.prop from the build to hash.

Change-Id: I7b50fbc3b1ea69781431a339ea5a7c3c049ade3b
Signed-off-by: RenanQueiroz <queirozrrq@gmail.com>

msm8226.mk
@@ -100,6 +100,9 @@ PRODUCT_PACKAGES += \
     libshims_sensorlistener \
     Camera2
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/hacks/hdrhax:system/vendor/etc/hdrhax
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images

sub77/android_device_motorola_msm8226-common

msm8226.mk
@@ -102,6 +102,9 @@ PRODUCT_PACKAGES += \
     libshims_sensorlistener \
     Snap
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hdrhax:system/vendor/etc/hdrhax
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images