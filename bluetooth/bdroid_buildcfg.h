1bc171ae7dea58ee417f621b729d3cd4ec3e527f
sub77 <sub77@ymail.com>
2018-10-26 06:30:39 +0000

    msm8226-common: fix2 bt

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,11 +18,30 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Moto G"
+#include <cutils/properties.h>
+#include <string.h>
+
+inline const char* BtmGetDefaultName()
+{
+	char device[PROPERTY_VALUE_MAX];
+	property_get("ro.xpe.device", device,"");
+
+	if (strcmp("peregrine", device)){
+		return "Moto G 4G";
+	} else if (strcmp("titan", device)){
+		return "Moto G 2014";
+	} else {
+		return "Moto G";
+	}
+}
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 #define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_L2CAP_CHANNELS    16
+#define BLE_VND_INCLUDED   TRUE
 // skips conn update at conn completion
 #define BTA_BLE_SKIP_CONN_UPD  FALSE
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #endif
