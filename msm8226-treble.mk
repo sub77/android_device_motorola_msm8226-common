f8c3cc037baf078b0c588d16e489e8347250d621
sub77 <sub77@ymail.com>
2018-11-23 13:11:33 +0100

    build binderized android.hardware.media.omx@1.0-impl

--- msm8226-treble.mk
+++ msm8226-treble.mk
@@ -54,6 +54,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl
 
+# OMX
+    android.hardware.media.omx@1.0-impl \
+    android.hardware.media.omx@1.0-service
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
